a8634b8b0d274bb09dfd70942d86f1bb21ccb4f2
edu <vizuete.eduardo@gmail.com>
2017-11-10 18:47:22 -0500

    Material deign card messages list

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ApiService {
+    messages = [];
     constructor ( private http: Http) {}
 
     getMessages() {
         this.http.get('http://localhost:3000/posts').subscribe(res => {
+            this.messages = res.json();
             console.log(res);
         });
     }
